bf8d92e64409044aba2e10fec1408b7b41a98685
Math012 <math111.0811@gmail.com>
2025-02-07 13:28:17 -0300

    Implementando lambda functions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.List;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<User> users = List.of(new User("Matheus", 26)
+                , new User("Ana", 28)
+                , new User("Julia", 30)
+                , new User("Marcos", 34)
+        );
+
+        printStringValue(user -> String.valueOf(user.getIdade()), users);
+        System.out.println("======================================");
+    }
+
+    private static void printStringValue(Function<User, String> callback, List<User> users){
+        users.forEach(u -> System.out.println(callback.apply(u)));
     }
 }
